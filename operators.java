2c5abfbec54d60325e3644909a3a17207a99cc3e
Bommineedupurnasai <bomminedupurnasai@gmail.com>
2025-06-19 15:09:51 +0530

    Update operators.java

--- operators.java
+++ operators.java
@@ -10,3 +10,30 @@ class Operators{
         System.out.println("a%b="+(a%b));
     }
 }
+//inc and dec
+public class IncDec {
+    static int increment(int x) { return ++x; }     
+    static int decrement(int x) { return --x; }      
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Original: " + a);
+        a = increment(a);
+        System.out.println("After increment: " + a);
+        a = decrement(a);
+        System.out.println("After decrement: " + a);
+    }
+}
+//Equal or not
+public class EqualOrNot {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        
+        if (a == b) {
+            System.out.println("a is equal to b");
+        } 
+        if (a != b) {
+            System.out.println("a is not equal to b  ");
+        } 
+    }
+}
+
