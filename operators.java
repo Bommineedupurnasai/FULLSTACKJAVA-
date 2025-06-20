286a89bce628f79aaf3c13f6125c9d9748d1aaa1
Bommineedupurnasai <bomminedupurnasai@gmail.com>
2025-06-20 14:21:18 +0530

    Update operators.java

--- operators.java
+++ operators.java
@@ -36,4 +36,55 @@ public class EqualOrNot {
         } 
     }
 }
+//Logical Operators
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b: " + (a && b)); 
+        System.out.println("a || b: " + (a || b)); 
+        System.out.println("!a: " + (!a));         
+        System.out.println("!b: " + (!b));        
+    }
+}
+//Relational Operators
+import java.util.Scanner;
+public class RelationalOperators {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.println("\nRelational Operator Results:");
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a <= b : " + (a <= b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a >= b : " + (a >= b));
+        scanner.close();
+    }
+}
+//smaller and larger Number
+import java.util.Scanner;
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        if (num1 < num2) {
+            System.out.println("Smaller number is: " + num1);
+            System.out.println("Larger number is: " + num2);
+        } else if (num1 > num2) {
+            System.out.println("Smaller number is: " + num2);
+            System.out.println("Larger number is: " + num1);
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+
+        sc.close();
+    }
+}
+
 
